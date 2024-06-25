b677c40e8c2e3a5a571a880f7eecf331e6b4c18f
Spencer Klessens <klessens92@gmail.com>
2024-06-25 15:39:50 -0500

    changes to App.css and new class .headBaner.  NavbarToggle, buttons and img icon are center with resolution for mobile to XL

--- src/components/Headder.js
+++ src/components/Headder.js
@@ -6,64 +6,65 @@ import Logo from '../app/assets/imag/image2.png';
 const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false);
     return (
-        <Navbar dark color='primary'  expand='md'>
-            <Row xs={12} sm={12} md={8} lg={12} lx={12} >
-                <Col lg={4} lx={4}></Col>
-                <Col xs={12} sm={12} md={12} lg={8} lx={8}>
+        <Navbar dark color='primary'  expand='md' >
+            <Row className='headBaner'>
+                <Col xs={12} sm={12} md={11} lg={10} xl={7}>
                     <NavbarBrand href='/' >
                         <img src={Logo} alt='logo not found'  className = 'headerImage'/>
                     </NavbarBrand>
                 </Col>
-                <Col lg={4} lx={4}></Col>
             
+
                 <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
             
                 <Collapse isOpen={menuOpen} navbar>
-                    <Nav navbar>
-                    <Col lg='10'></Col>
-                    <Col md={3} lg={3} lx={3} >
-                        <NavItem className='link-box'>
-                            <NavLink className='nav-link' to='/'>
-                                <i className='fa fa-home fa-lg' /> Home
-                            </NavLink>
-                        </NavItem>
-                    </Col>
-                    <Col md={3} lg={3} lx={3}>
-                        <NavItem className='link-box'>
-                            <NavLink className='nav-link' to='/program'>
-                                <i className='fa fa-th-list fa-lg' /> Program
-                            </NavLink>
-                        </NavItem>
-                    </Col>
-                    <Col md={3} lg={3} lx={3} >
-                        <NavItem className='link-box'>
-                            <NavLink className='nav-link' to='/story'>
-                                <i className='fa fa-book fa-lg' /> Story
-                            </NavLink>
-                        </NavItem>
-                    </Col>
-                    <Col md={3} lg={3} lx={3} >
-                        <NavItem className='link-box'>
-                            <NavLink className='nav-link' to='/store'>
-                                <i className='fa  fa-shopping-bag' /> Store
-                            </NavLink>
-                        </NavItem>
-                    </Col>
-                    <Col md={3} lg={3} lx={3} >
-                        <NavItem className='link-box'>
-                            <NavLink className='nav-link' to='/contact'>
-                                <i className='fa fa-address-card fa-lg' /> Contact
-                            </NavLink>
-                        </NavItem>
-                    </Col>
-                    <Col md={3} lg={3} lx={3} >   
-                        <NavItem className='link-box'>
-                            <NavLink className='nav-link' to='/cart'>
-                                <i className='fa fa-shopping-cart fa-lg' /> Cart
-                            </NavLink>
-                        </NavItem>
-                    </Col>
-                    <Col lg='11'></Col>
+                    <Nav navbar className='headBaner'>
+                    
+                        
+                        <Col xs={12} sm={12} md={2} lg={2} xl={2} >
+                            <NavItem className='link-box'>
+                                <NavLink className='nav-link' to='/'>
+                                    <i className='fa fa-home fa-lg' /> Home
+                                </NavLink>
+                            </NavItem>
+                        </Col>
+                        <Col xs={12} sm={12} md={2} lg={2} xl={2}>
+                            <NavItem className='link-box'>
+                                <NavLink className='nav-link' to='/program'>
+                                    <i className='fa fa-th-list fa-lg' /> Program
+                                </NavLink>
+                            </NavItem>
+                        </Col>
+                        <Col xs={12} sm={12} md={2} lg={2} xl={2}>
+                            <NavItem className='link-box'>
+                                <NavLink className='nav-link' to='/story'>
+                                    <i className='fa fa-book fa-lg' /> Story
+                                </NavLink>
+                            </NavItem>
+                        </Col>
+                        <Col xs={12} sm={12} md={2} lg={2} xl={2}>
+                            <NavItem className='link-box'>
+                                <NavLink className='nav-link' to='/store'>
+                                    <i className='fa  fa-shopping-bag' /> Store
+                                </NavLink>
+                            </NavItem>
+                        </Col>
+                        <Col xs={12} sm={12} md={2} lg={2} xl={2}>
+                            <NavItem className='link-box'>
+                                <NavLink className='nav-link' to='/contact'>
+                                    <i className='fa fa-address-card fa-lg' /> Contact
+                                </NavLink>
+                            </NavItem>
+                        </Col>
+                        <Col  xs={12} sm={12} md={2} lg={2} xl={2}>   
+                            <NavItem className='link-box'>
+                                <NavLink className='nav-link' to='/cart'>
+                                    <i className='fa fa-shopping-cart fa-lg' /> Cart
+                                </NavLink>
+                            </NavItem>
+                        </Col>
+                        
+                    
                     </Nav>
                 </Collapse>
             </Row>
